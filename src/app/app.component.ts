5a19b675e3d3ce74cf3c454a879c65c27234b543
Vinicius Soares <viniciussoaresaqw@hotmail.com>
2022-07-12 18:30:01 -0300

    first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<app-menu><router-outlet></router-outlet></app-menu>'
 })
 export class AppComponent {
   title = 'primeiroDashboard';
